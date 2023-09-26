ada2e09fa5ffa65755ff6c0d823c746cb331f7ed
joel <joel.vinansaca@tecnologicoliceocristiano.edu.ec>
2023-09-25 21:13:56 -0500

    comit inicial

---
+++ IntroDevOps/IntroDevOps.Tests/MathTests.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace IntroDevOps.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestSumar(int num1 int num2)
+        {
+            //Arranque
+            var expected = 10;
+
+            //Act
+            var resultado = Calculadora.Sumar(num1, num2);
+
+            //Asser
+            Assert.Equal()
+        }
+    }
+}
---
+++ IntroDevOps/IntroDevOps.math/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿namespace IntroDevOps.math
+{
+    public class Calculadora
+    {
+        public static int sumar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Restar(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
